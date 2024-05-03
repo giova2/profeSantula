82f3fdc397237e8095b29bd3a91c4e7fbdc35542
franco giovannangelo <fcgiova@gmail.com>
2024-05-03 11:07:08 -0300

    remove old example

--- POC-1/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Entity} from "./types";
-import { entityGenerator } from "./utils/factory"
-import { executeRightFactory } from "./utils/strategy";
-
-const [sectionFormEntity, sectionDisplayEntity]: Entity[] = entityGenerator('section')
-const [areaFormEntity, areaDisplayEntity]: Entity[] = entityGenerator('area', [sectionFormEntity, sectionDisplayEntity])
- 
-executeRightFactory(areaFormEntity)
-executeRightFactory(areaDisplayEntity)
